e82714a839df93a0047cf0335801d8ec028993bd
MezzMar <gary.n.1999@gmail.com>
2020-07-24 14:54:13 +0000

    .gitignore db

---
+++ build/precache-manifest.24856d1ad40fc2a053353a23fb0bc855.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e4a51e819576f14fbb6f819a96f32bb",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5f76a6a26129142f5e2f",
+    "url": "./static/css/main.4e92f82f.chunk.css"
+  },
+  {
+    "revision": "223fbcb014ac0f99d1d8",
+    "url": "./static/js/2.e5346c2d.chunk.js"
+  },
+  {
+    "revision": "76bfb6026d4f39dfdbd9858feb1ac76f",
+    "url": "./static/js/2.e5346c2d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f76a6a26129142f5e2f",
+    "url": "./static/js/main.565157d6.chunk.js"
+  },
+  {
+    "revision": "58b6b1513dc2f8fc42c5",
+    "url": "./static/js/runtime-main.783f1167.js"
+  },
+  {
+    "revision": "276dfa45952d11f3281ce001ae185e9e",
+    "url": "./static/media/Paomedia-Small-N-Flat-Book.276dfa45.svg"
+  },
+  {
+    "revision": "ca4a83c01a287bbda3e657a21e01052a",
+    "url": "./static/media/Paomedia-Small-N-Flat-File-text.ca4a83c0.svg"
+  },
+  {
+    "revision": "1d8fe391f05a450037823a8a4d8b9307",
+    "url": "./static/media/Papirus-Team-Papirus-ImageGeneric.1d8fe391.svg"
+  },
+  {
+    "revision": "f316c01e677b9cce34e32c1f51bddd6e",
+    "url": "./static/media/Papirus-Team-Papirus-Mimetypes-App-x-javascript.f316c01e.svg"
+  },
+  {
+    "revision": "71a3d0c14a2beb61c241be0f752d6806",
+    "url": "./static/media/Papirus-Team-Papirus-Mimetypes-App-x-msdos-program.71a3d0c1.svg"
+  },
+  {
+    "revision": "15c53d6e62690d48018b0acbdb49c35f",
+    "url": "./static/media/Papirus-Team-Papirus-Mimetypes-Text-x-java.15c53d6e.svg"
+  },
+  {
+    "revision": "d52fc9263559d42fcf8dcbb8bc49cdce",
+    "url": "./static/media/Papirus-Team-Papirus-Mimetypes-X-office-document.d52fc926.svg"
+  },
+  {
+    "revision": "d209dc933df7ec5f141ebfe3ee4eef32",
+    "url": "./static/media/Papirus-Team-Papirus-Mimetypes-X-office-presentation.d209dc93.svg"
+  },
+  {
+    "revision": "fc0d7531e98bc9a25fbbd6c04a799a7a",
+    "url": "./static/media/Papirus-Team-Papirus-Mimetypes-X-office-spreadsheet.fc0d7531.svg"
+  },
+  {
+    "revision": "4dbf7341687bd6c0802de28f6fef0bc5",
+    "url": "./static/media/Papirus-Team-Papirus-audio.4dbf7341.svg"
+  },
+  {
+    "revision": "4816869667b2b669dd0683dbec08852b",
+    "url": "./static/media/Papirus-Team-Papirus-compress.48168696.svg"
+  },
+  {
+    "revision": "8b8629d0665bcae9c6f1fdbfe63350e8",
+    "url": "./static/media/Papirus-Team-Papirus-html.8b8629d0.svg"
+  },
+  {
+    "revision": "6032e341329394c3dfcf7da9c11e0797",
+    "url": "./static/media/Papirus-Team-Papirus-iso.6032e341.svg"
+  },
+  {
+    "revision": "4dde535184d84c913d4448d0f605dd12",
+    "url": "./static/media/Papirus-Team-Papirus-json.4dde5351.svg"
+  },
+  {
+    "revision": "5fd1241c0d9bf6dd2f648a141461ea3a",
+    "url": "./static/media/Papirus-Team-Papirus-pdf.5fd1241c.svg"
+  },
+  {
+    "revision": "41a44ff7c589eed1203a0352af611cb9",
+    "url": "./static/media/Papirus-Team-Papirus-video.41a44ff7.svg"
+  },
+  {
+    "revision": "a1a2034fd740f745d85894c94dfa7721",
+    "url": "./static/media/Papirus-Team-Papirus-zerosize.a1a2034f.svg"
+  },
+  {
+    "revision": "ad58eeeaf865d37f0961e86b92a8062c",
+    "url": "./static/media/background_study_by_hibelton_dc28kuo-fullview.ad58eeea.jpg"
+  },
+  {
+    "revision": "78445f96e1021bcbadba37ade602eb84",
+    "url": "./static/media/folder.78445f96.svg"
+  }
+]);
